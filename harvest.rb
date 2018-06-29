4a0418cc6193270613dd8a25c68c7c48b8d24af8
Steve Gattuso <steve@stevegattuso.me>
2018-06-29 15:17:11 -0400

    cleaning up

--- harvest.rb
+++ harvest.rb
@@ -1,17 +1,22 @@
 require 'http'
 require 'date'
 require 'json'
+require 'yaml'
 
 # Create new token on harvest dev account to get your credentials 
-HARVEST_TOKEN = ''
-HARVEST_ACCOUNT_ID = 123456
+ROOT_PATH = File.expand_path(File.dirname(__FILE__))
+CONFIG = YAML.load_file(File.join(ROOT_PATH, 'config.yml'))
 
 def generate_request
   HTTP.headers({
-    'Authorization' => "Bearer #{HARVEST_TOKEN}",
-    'Harvest-Account-Id' => HARVEST_ACCOUNT_ID,
+    'Authorization' => "Bearer #{CONFIG['harvest_token']}",
+    'Harvest-Account-Id' => CONFIG['harvest_account_id'],
     'User-Agent' => 'BetterTouchTool (steve@stevegattuso.me)'
   })
+end
+
+def icon_path(active:)
+  File.join(ROOT_PATH, "resources/harvest-#{active ? 'active' : 'inactive'}.png")
 end
 
 def get_latest_timer
@@ -57,9 +62,9 @@ def toggle_latest
   minutes = ((latest['hours'] - hours) * 60).ceil
 
   puts JSON.generate({
-    text: "#{hours}:#{minutes}",
+    text: "#{hours}:#{minutes.to_s.rjust(2, '0')}",
     background_color: '0, 0, 0, 0',
-    icon_path: "~/Code/scripts/harvest-#{!latest['is_running'] ? 'active' : 'inactive'}.png", 
+    icon_path: icon_path(active: latest['is_running']), 
   })
 end
 
@@ -71,22 +76,32 @@ def generate_status
   minutes = ((latest['hours'] - hours) * 60).ceil
 
   puts JSON.generate({
-    text: "#{hours}:#{minutes}",
+    text: "#{hours}:#{minutes.to_s.rjust(2, '0')}",
     background_color: '0, 0, 0, 0',
-    icon_path: "~/Code/scripts/harvest-#{latest['is_running'] ? 'active' : 'inactive'}.png", 
+    icon_path: icon_path(active: latest['is_running']), 
   })
 end
 
 def generate_err
   puts JSON.generate({
-    text: "--",
+    text: "--:--",
     background_color: '0, 0, 0, 0',
-    icon_path: "~/Code/scripts/harvest-inactive.png", 
+    icon_path: icon_path(active: false),
   })
 end
 
 if ARGV[0] == '-t'
   toggle_latest
+elsif ARGV[0] == '-j'
+  config = File.read(File.join(ROOT_PATH, 'resources/bettertouchtool.json'))
+  json = JSON.parse(config)
+
+  json['BTTShellTaskActionScript'] =
+    "#{RbConfig.ruby} #{ROOT_PATH}/harvest.rb -t"
+  json['BTTTriggerConfig']['BTTTouchBarShellScriptString'] =
+    "#{RbConfig.ruby} #{ROOT_PATH}/harvest.rb"
+
+  puts JSON.generate(json)
 else
   generate_status
 end
